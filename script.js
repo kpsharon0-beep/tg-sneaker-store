e69e534820a84c624e904654b3baf2db03f24cb1
kpsharon0-beep <kpsharon0@gmail.com>
2026-03-23 06:55:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,27 @@ const products=[
 {name:"Jordan Dior High",price:1999,mrp:3999,brand:"Nike",img:"jordan1.jpg",desc:"Luxury"},
 {name:"Nike Dunk Ghost Rider",price:2000,mrp:3999,brand:"Nike",img:"gostrider.jpg",desc:"Dark"},
 {name:"Nike Chunky Dunky",price:1999,mrp:3999,brand:"Nike",img:"chunky.jpg",desc:"Colorful"},
-{name:"nike air jordan 1 retro low zion williamson voodoo price",price:3499,mrp:4999,brand:"Nike",img:"retro11.jpg",desc:"rare"},
+{
+name:"Jordan Low Year of Dragon",
+price:1999,
+mrp:3999,
+brand:"Nike",
+img:"dragon1.jpg",
+stock:3,
+desc:"Jordan Low Year of Dragon edition. Rare design with bold premium finish.",
+reviews:[
+"🔥 insane design",
+"rare piece",
+"clean af",
+"worth it",
+"premium look",
+"🔥🔥",
+"top quality",
+"must cop",
+"unique",
+"10/10"
+]
+},
   
 {name:"RedTape Lifestyle",price:1999,mrp:2999,brand:"RedTape",img:"radtape-lifestyle.jpg"},
 {name:"RedTape Casual",price:1899,mrp:2799,brand:"RedTape",img:"redtape-casual.jpg"},
